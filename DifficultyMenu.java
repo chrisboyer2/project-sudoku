c3d46b3d0cf15895ebc87e98cda4ebe052c6e10c
chrisboyer2 <njlt@iup.edu>
2015-11-11 11:23:13 -0500

    Update DifficultyMenu.java

--- DifficultyMenu.java
+++ DifficultyMenu.java
@@ -8,18 +8,27 @@ import java.awt.event.ActionEvent;
 public class DifficultyMenu {
 	JPanel p = new JPanel();
 	
-	DifficultyMenu(){
+	DifficultyMenu(final boolean timeAttackMode){
 		p.setLayout(null);
 		p.setSize(690,590);
 		
 		JButton easyButton = new JButton("Easy");
 		easyButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				PlayScreen play = new PlayScreen();
-				Sudoku.frame.getContentPane().removeAll();
-				Sudoku.frame.getContentPane().repaint();
-				Sudoku.frame.getContentPane().add(play.p, BorderLayout.CENTER);
-				play.play(15);
+				if(!timeAttackMode){
+					PlayScreen play = new PlayScreen();
+					Sudoku.frame.getContentPane().removeAll();
+					Sudoku.frame.getContentPane().repaint();
+					Sudoku.frame.getContentPane().add(play.p, BorderLayout.CENTER);
+					play.play(15);
+				}
+				else{
+					TAttackScreen playT = new TAttackScreen();
+					Sudoku.frame.getContentPane().removeAll();
+					Sudoku.frame.getContentPane().repaint();
+					Sudoku.frame.getContentPane().add(playT.p, BorderLayout.CENTER);
+					playT.play(15);
+				}
 			}
 		});
 		easyButton.setBounds(282, 195, 89, 23);
@@ -28,11 +37,20 @@ public class DifficultyMenu {
 		JButton mediumButton = new JButton("Medium");
 		mediumButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				PlayScreen play = new PlayScreen();
-				Sudoku.frame.getContentPane().removeAll();
-				Sudoku.frame.getContentPane().repaint();
-				Sudoku.frame.getContentPane().add(play.p, BorderLayout.CENTER);
-				play.play(25);
+				if(!timeAttackMode){
+					PlayScreen play = new PlayScreen();
+					Sudoku.frame.getContentPane().removeAll();
+					Sudoku.frame.getContentPane().repaint();
+					Sudoku.frame.getContentPane().add(play.p, BorderLayout.CENTER);
+					play.play(25);
+				}
+				else{
+					TAttackScreen playT = new TAttackScreen();
+					Sudoku.frame.getContentPane().removeAll();
+					Sudoku.frame.getContentPane().repaint();
+					Sudoku.frame.getContentPane().add(playT.p, BorderLayout.CENTER);
+					playT.play(25);
+				}
 			}
 		});
 		mediumButton.setBounds(282, 221, 89, 23);
@@ -41,11 +59,20 @@ public class DifficultyMenu {
 		JButton hardButton = new JButton("Hard");
 		hardButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				PlayScreen play = new PlayScreen();
-				Sudoku.frame.getContentPane().removeAll();
-				Sudoku.frame.getContentPane().repaint();
-				Sudoku.frame.getContentPane().add(play.p, BorderLayout.CENTER);
-				play.play(35);
+				if(!timeAttackMode){
+					PlayScreen play = new PlayScreen();
+					Sudoku.frame.getContentPane().removeAll();
+					Sudoku.frame.getContentPane().repaint();
+					Sudoku.frame.getContentPane().add(play.p, BorderLayout.CENTER);
+					play.play(35);
+				}
+				else{
+					TAttackScreen playT = new TAttackScreen();
+					Sudoku.frame.getContentPane().removeAll();
+					Sudoku.frame.getContentPane().repaint();
+					Sudoku.frame.getContentPane().add(playT.p, BorderLayout.CENTER);
+					playT.play(35);
+				}
 			}
 		});
 		hardButton.setBounds(281, 248, 89, 23);
@@ -54,11 +81,20 @@ public class DifficultyMenu {
 		JButton expertButton = new JButton("Expert");
 		expertButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				PlayScreen play = new PlayScreen();
-				Sudoku.frame.getContentPane().removeAll();
-				Sudoku.frame.getContentPane().repaint();
-				Sudoku.frame.getContentPane().add(play.p, BorderLayout.CENTER);
-				play.play(45);
+				if(!timeAttackMode){
+					PlayScreen play = new PlayScreen();
+					Sudoku.frame.getContentPane().removeAll();
+					Sudoku.frame.getContentPane().repaint();
+					Sudoku.frame.getContentPane().add(play.p, BorderLayout.CENTER);
+					play.play(45);
+				}
+				else{
+					TAttackScreen playT = new TAttackScreen();
+					Sudoku.frame.getContentPane().removeAll();
+					Sudoku.frame.getContentPane().repaint();
+					Sudoku.frame.getContentPane().add(playT.p, BorderLayout.CENTER);
+					playT.play(45);
+				}
 			}
 		});
 		expertButton.setBounds(282, 275, 89, 23);
@@ -68,3 +104,5 @@ public class DifficultyMenu {
 	}
 	
 }
+
+
